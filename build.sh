da981babe5d12b183890e8aa3b1e50273c86e643
Jason Barrie Morley <jbmorley@mac.com>
2014-01-18 21:29:03 +0100

    Updating the build script.

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 ./generate_resources.py src/nezumi.json appinfo-template.json . || exit 1
-pebble clean
-pebble build
+pebble clean || exit 1
+pebble build || exit 1
+pebble install --phone $1
