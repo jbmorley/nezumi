31f98d40fa23751f02f73341ffcf9e3b786d4da4
Jason Barrie Morley <jbmorley@mac.com>
2014-01-18 22:59:10 +0100

    Fixing the build script and updating the animation timing.

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-./generate_resources.py src/nezumi.json appinfo-template.json . || exit 1
+./generate_classes.py src/types.json src/data.json > src/animation.h || exit 1
 pebble clean || exit 1
 pebble build || exit 1
 pebble install --phone $1
