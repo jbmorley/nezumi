a7f048f239f26f924ff972ce3a8eacae97e46f69
Jason Barrie Morley <jbmorley@mac.com>
2014-01-19 08:28:29 +0100

    Cleaning up the directory structure a little.

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-tools/addimages resources/images/ appinfo-template.json .
-tools/makestruct src/types.json src/data.json src/animation.h || exit 1
+tools/addimages resources/images/ appinfo-template.json . || exit 1
+tools/makestruct json/types.json json/data.json src/animation.h || exit 1
 pebble clean || exit 1
 pebble build || exit 1
 pebble install --phone $1
