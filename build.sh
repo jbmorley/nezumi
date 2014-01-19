2e85b39ae1f34a43815817528173b6f2cdeef96d
Jason Barrie Morley <jbmorley@mac.com>
2014-01-19 08:25:38 +0100

    Cleaning up the tooling a little.

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+tools/addimages resources/images/ appinfo-template.json .
 tools/makestruct src/types.json src/data.json src/animation.h || exit 1
 pebble clean || exit 1
 pebble build || exit 1
