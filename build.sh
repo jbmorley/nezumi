7268f31f85126d37c58d3c9b66e7fc50941349c8
Jason Barrie Morley <jbmorley@mac.com>
2014-01-19 08:10:20 +0100

    Updating the build tool to use the new script.

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-./generate_classes.py src/types.json src/data.json > src/animation.h || exit 1
+tools/makestruct src/types.json src/data.json src/animation.h || exit 1
 pebble clean || exit 1
 pebble build || exit 1
 pebble install --phone $1
