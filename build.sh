97fbb119a3cd85547000dc5002ccb05f9f2baaad
Jason Barrie Morley <jbmorley@mac.com>
2015-05-03 10:32:34 +0200

    Switching to a python build script.

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-tools/addimages resources/images/ appinfo-template.json . || exit 1
-tools/makestruct json/types.json json/data.json src/animation.h || exit 1
-pebble clean || exit 1
-pebble build || exit 1
-pebble install --phone $1
