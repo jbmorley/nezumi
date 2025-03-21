68d905bf438a4870255305e1764aa81fb6c604ed
Jason Morley <hello@jbmorley.co.uk>
2025-03-21 11:16:14 -1000

    Initial commit of a Rust + raylib implementation

---
+++ rust/src/main.rs
@@ -0,0 +1,121 @@
+use serde::Deserialize;
+use std::collections::HashMap;
+use std::fs;
+use raylib::prelude::*;
+
+#[derive(Debug, Deserialize)]
+struct Frame {
+    // TODO: Rename to file_name?
+    file: String,
+    duration: f64,
+}
+
+#[derive(Debug, Deserialize)]
+struct Operation {
+    set: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct State {
+    frames: Vec<Frame>,
+    actions: HashMap<String, Operation>,
+}
+
+type FrameMap = HashMap<String, raylib::core::texture::Texture2D>;
+
+fn main() {
+    println!("Hello, world!");
+
+    let contents = fs::read_to_string("states.json")
+        .expect("Unable to read animation data");
+    println!("{}", contents);
+
+    let state: HashMap<String, State> = serde_json::from_str(&contents)
+        .expect("Invalid animation data");
+
+    let (mut rl, thread) = raylib::init()
+        .size(640, 480)
+        .title("Nezumi")
+        .build();
+
+    rl.set_target_fps(60);
+
+    let mut frames: FrameMap = FrameMap::new();
+
+    // Cache all the frames.
+    let paths = fs::read_dir("frames")
+        .expect("Failed to list frames");
+    for path in paths {
+        let a = path.unwrap().path();
+        let b = a.to_str().unwrap();
+        let image = Image::load_image(&b)
+            .expect("Failed to load image");
+        let texture = rl.load_texture_from_image(&thread, &image)
+            .expect("Failed to create texture");
+        frames.insert(a.file_name().unwrap().to_str().unwrap().to_owned(), texture);
+    }
+
+    let frame_duration = 0.3;
+    let scale = 6.0;
+    
+    let mut frame = 0;
+    let mut accumulator = 0.0;
+    let mut current_state_name = "blink";
+    let mut action = "default";  // TODO: Unnecessary?
+
+    // Gestures.
+    let mut current_gesture = raylib::consts::Gesture::GESTURE_NONE;
+
+    while !rl.window_should_close() {
+        let mut current_state = &state[current_state_name];
+        let frame_count = current_state.frames.len();
+
+        // Handle gestures.
+        current_gesture = rl.get_gesture_detected();
+        if current_gesture == raylib::consts::Gesture::GESTURE_DRAG {
+            action = "drag";
+        } else if current_gesture == raylib::consts::Gesture::GESTURE_TAP {
+            action = "tap";
+        } else if current_gesture == raylib::consts::Gesture::GESTURE_HOLD {
+            // Ideally a hold gesture would clear the effect of the tap, but
+            // Raylib always seems to give us a 'hold' after a tap, so we can't
+            // easily differentiate between these two states.
+            action = "tap";
+        } else {
+            // action = "default";
+        }
+
+        accumulator += rl.get_frame_time();
+        while accumulator > frame_duration {
+            accumulator -= frame_duration;
+            frame += 1;
+
+            // Handle state transitions if we've reached the end of the
+            // current animation loop.
+            if frame >= frame_count {
+                frame = 0;
+                current_state_name = &current_state.actions[action].set;
+                current_state = &state[current_state_name];
+
+                // Reset the action.
+                action = "default";
+            }
+        }
+
+        let frame_name = &current_state.frames[frame].file;
+
+        // println!("{}, {}", frame, frame_name);
+
+        let mut d = rl.begin_drawing(&thread);
+        d.clear_background(Color::WHITE);
+
+        let texture = &frames[frame_name];
+        d.draw_texture_ex(
+            texture,
+            Vector2 {x: 0.0, y: 0.0},
+            0.0,
+            scale,
+            Color::WHITE);
+        
+    }
+}
