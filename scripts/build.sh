f6173cbf33798550c0f719dc1bdb8009d9f3db62
Jason Morley <hello@jbmorley.co.uk>
2026-09-10 19:03:31 +0100

    Use an in-tree mise install

--- scripts/build.sh
+++ scripts/build.sh
@@ -25,8 +25,6 @@ set -o pipefail
 set -x
 set -u
 
-echo "PATH=$PATH"
-
 ROOT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." &> /dev/null && pwd )"
 SCRIPTS_DIRECTORY="$ROOT_DIRECTORY/scripts"
 SOURCE_DIRECTORY="$ROOT_DIRECTORY/rust"
