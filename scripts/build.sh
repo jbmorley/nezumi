a33138b7ca5996b80291f188897f9f2e0fc2de05
Jason Morley <hello@jbmorley.co.uk>
2026-09-11 09:35:34 +0100

    ci: ⚙️ Pin the GitHub CLI to a specific version
    This change pins the GitHub CLI (`gh`) to 2.100.0; part of a wider experiment to see if explicitly pinning and managing dependencies will lead to more stable and predictable builds and update lifecycles.

--- scripts/build.sh
+++ scripts/build.sh
@@ -29,9 +29,6 @@ ROOT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." &> /dev/null && pwd 
 SCRIPTS_DIRECTORY="$ROOT_DIRECTORY/scripts"
 SOURCE_DIRECTORY="$ROOT_DIRECTORY/rust"
 
-# Ensure required commands are available.
-which cmake || (echo "CMake (cmake) not available on the path." && exit 1)
-which gh || (echo "GitHub cli (gh) not available on the path." && exit 1)
 
 "$SCRIPTS_DIRECTORY/generate-credits" \
     "$ROOT_DIRECTORY/graphics/credits.png" \
